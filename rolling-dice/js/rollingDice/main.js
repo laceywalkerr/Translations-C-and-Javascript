d7acb11433695aa951be7557368a218c588df65b
Lacey Walker <bluelarksong@gmail.com>
2020-11-13 16:02:42 -0600

    started working on rolling dice. this one hits harder somehow.

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,6 +1,15 @@
 function main() {
-  // Put your code here
 
+  console.log("Let's roll some dice, baby!");
+  console.log("---------------------------")
+
+  for (let i = 0; i < 10; i++) {
+    console.log(`The number is ${i}`);
+  }
+
+  const die1 = die.roll()
+  const die2 = die.roll()
+  let message = `${die.dieFace(die1)} + ${die.dieFace(die2)} == ${die1 + die2}`
 }
 
 main();
