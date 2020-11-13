9d61c5a4eabc8e0c3530a2c0a05460decf251ae5
Lacey Walker <bluelarksong@gmail.com>
2020-11-13 15:21:05 -0600

    enemies translated

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,52 @@
 function main() {
-  // Put your code here
+
+  console.log("My Enemies List!");
+  console.log("----------------");
+
+  const enemies = [
+    {
+      firstName: "Ramona",
+      lastName: "Flowers",
+      offences: ["Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"],
+      isReallyHated: true
+    },
+
+    {
+      firstName: "Darth",
+      lastName: "Vader",
+      offences: ["Cut off luke's hand",
+        "Murdered all those kids",
+        "Unkind management practices"],
+      isReallyHated: false
+    },
+
+    {
+      firstName: "Betty",
+      lastName: "White",
+      offences: ["Phone calls in the theater",
+        "Phone calls on the bus",
+        "Phone calls in line at the grocery store",
+        "Poor conversationalist"],
+      isReallyHated: true
+    },
+
+    {
+      firstName: "Leon",
+      lastName: "Peck",
+      offences: ["Keeps giving me a hotplate"],
+      isReallyHated: false
+    }
+
+  ]
+
+  for (let enemy of enemies) {
+    if (enemy.isReallyHated === true) {
+      console.log(`${enemy.firstName} ${enemy.lastName} really really dislike! EW.`);
+    } else {
+      console.log(`${enemy.firstName} ${enemy.lastName} still greatly dislike, but don't loathe entirely.`);
+    }
+  }
 
 }
 
