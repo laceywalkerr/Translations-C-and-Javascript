a706834514d04e3096d2ee6d87a7c7dc3c742c80
Lacey Walker <bluelarksong@gmail.com>
2020-11-13 13:37:51 -0600

    first commit

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,23 @@
 function main() {
-  // Put your code here
+
+  const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  ];
+
+  for (let name of names) {
+    console.log(`Location ${name}`);
+  }
+
+  console.log("");
+
+  const theNames = names.filter(n => n.startsWith("The"))
+
+  console.log(theNames);
+
+  for (let name of theNames) {
+    console.log(`The Locations ${name}`);
+  }
 
 }
 
